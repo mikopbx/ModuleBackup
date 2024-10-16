a1148421d8b782167da32e1d9fcd07a3a82f861f
Nikolay Beketov <nbek@miko.ru>
2024-10-16 11:17:46 +0700

    Refactor checkbox usage
    Replaced redundant checkbox code with a new `addCheckBox` method across multiple forms for consistency and maintainability. Updated relevant dependencies and forms to ensure proper integration, and fixed minor issues in JavaScript files.

--- App/Forms/ModuleBackupCreateForm.php
+++ App/Forms/ModuleBackupCreateForm.php
@@ -1,10 +1,21 @@
 <?php
-/**
- * Copyright (C) MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Nikolay Beketov, 10 2019
+
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace Modules\ModuleBackup\App\Forms;
@@ -12,18 +23,30 @@ namespace Modules\ModuleBackup\App\Forms;
 use Phalcon\Forms\Element\Check;
 use Phalcon\Forms\Form;
 
-
 class ModuleBackupCreateForm extends Form
 {
-
     public function initialize($entity = null, $options = null)
     {
         foreach ($options as $name => $value) {
-            $cheskarr = ['value' => null];
-            if ($value) {
-                $cheskarr = ['checked' => 'checked', 'value' => null];
-            }
-            $this->add(new Check($name, $cheskarr));
+            $this->addCheckBox($name, intval($value) === 1);
         }
+    }
+
+    /**
+     * Adds a checkbox to the form field with the given name.
+     * Can be deleted if the module depends on MikoPBX later than 2024.3.0
+     *
+     * @param string $fieldName The name of the form field.
+     * @param bool $checked Indicates whether the checkbox is checked by default.
+     * @param string $checkedValue The value assigned to the checkbox when it is checked.
+     * @return void
+     */
+    public function addCheckBox(string $fieldName, bool $checked, string $checkedValue = 'on'): void
+    {
+        $checkAr = ['value' => null];
+        if ($checked) {
+            $checkAr = ['checked' => $checkedValue,'value' => $checkedValue];
+        }
+        $this->add(new Check($fieldName, $checkAr));
     }
 }
